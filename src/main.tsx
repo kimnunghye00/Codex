33dc8bffdafb33c4bd4fa45c20d9d5fafd4285bb
kimnunghye00 <kimnunghye00@gmail.com>
2026-08-30 10:52:27 +0900

    Render app through signup gate

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
+import Root from './Root';
 import './styles.css';
 import './auth-onboarding.css';
 
-createRoot(document.getElementById('root')!).render(<StrictMode><App /></StrictMode>);
+createRoot(document.getElementById('root')!).render(<StrictMode><Root /></StrictMode>);
