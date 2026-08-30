dd8cb37f52ef10fedc225d2bfa9be49b1f4fdb27
kimnunghye00 <kimnunghye00@gmail.com>
2026-08-30 11:03:30 +0900

    Load nickname styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,5 +3,6 @@ import { createRoot } from 'react-dom/client';
 import Root from './Root';
 import './styles.css';
 import './auth-onboarding.css';
+import './nickname.css';
 
 createRoot(document.getElementById('root')!).render(<StrictMode><Root /></StrictMode>);
