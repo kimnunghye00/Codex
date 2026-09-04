1d185c2e2c3bdb72d2bd5c4a4428b0f6824b84ec
kimnunghye00 <kimnunghye00@gmail.com>
2026-09-04 20:09:36 +0900

    Initialize native ROUTE shell on Android and iOS

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import Root from './Root';
+import { initializeNativeApp } from './lib/native';
 import './styles.css';
 import './auth-onboarding.css';
 import './nickname.css';
@@ -25,5 +26,7 @@ import './home-map-overlay.css';
 import './home-map-overlay';
 import './close-standard.css';
 import './more-enhance';
+
+void initializeNativeApp();
 
 createRoot(document.getElementById('root')!).render(<StrictMode><Root /></StrictMode>);
