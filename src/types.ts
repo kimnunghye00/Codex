a30b033310e6d37ad1ce9411237218b0da897071
kimnunghye00 <kimnunghye00@gmail.com>
2026-09-05 17:19:05 +0900

    Add memory draft type for cross-feature flows

--- src/types.ts
+++ src/types.ts
@@ -29,3 +29,5 @@ export type Memory = {
   createdBy: Sender;
   favorite?: boolean;
 };
+
+export type MemoryDraft = Pick<Memory, 'title' | 'date' | 'description'> & Partial<Pick<Memory, 'location' | 'tags'>>;
