2a18435208863370f390de898fa0af306ceb309c
kimnunghye00 <kimnunghye00@gmail.com>
2026-09-05 19:18:11 +0900

    Refresh web cache for approved split home layout

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'route-web-v6';
+const CACHE_NAME = 'route-web-v7';
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.add('./')).catch(() => undefined));
