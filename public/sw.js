b7a5aa271305ce0ea010b2e2bd3d21578cffdb27
kimnunghye00 <kimnunghye00@gmail.com>
2026-09-13 20:07:26 +0900

    Refresh home anniversary cache

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'danduli-web-v14-mute-icon-toggle';
+const CACHE_NAME = 'danduli-web-v15-home-anniversary';
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.add('./')).catch(() => undefined));
